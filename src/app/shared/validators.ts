0e7a52c85b9de5b86636ad7a84648abb84dbc5b1
Moumen Harmoush <moumen.harmoush@isento.de>
2023-03-14 11:13:34 +0100

    fixing validator bug

--- src/app/shared/validators.ts
+++ src/app/shared/validators.ts
@@ -1,14 +1,37 @@
 import { AbstractControl, FormControl, ValidatorFn } from "@angular/forms";
 
-export function compareValuesValidator(control1: FormControl, control2: FormControl): ValidatorFn {
+export function compareValuesPulseValidator(control1: FormControl, control2: FormControl): ValidatorFn {
     return (formControl: AbstractControl) => {
   
       if (control1.value >= control2.value) {
         control1.setErrors({ 'notGreaterThan': true });
         return { 'notGreaterThan': true };
       } else {
-        control1.setErrors(null);
-        return null;
+        if (control2.value >= 0 && control1.value >= 0){
+          control1.setErrors(null);
+          return null;
+        } else {
+          control1.setErrors({ 'error': true });
+          return { 'error': true };
+        }
       }
     };
+}
+
+export function compareValuesDegreeValidator(control1: FormControl, control2: FormControl): ValidatorFn {
+  return (formControl: AbstractControl) => {
+
+    if (control1.value >= control2.value) {
+      control1.setErrors({ 'notGreaterThan': true });
+      return { 'notGreaterThan': true };
+    } else {
+      if (control2.value >= -9000 && control1.value >= -9000){
+        control1.setErrors(null);
+        return null;
+      } else {
+        control1.setErrors({ 'error': true });
+        return { 'error': true };
+      }
+    }
+  };
 }
