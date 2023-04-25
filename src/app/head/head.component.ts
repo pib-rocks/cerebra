08a5d4b00c5a3f1c47422d0c27529027ebee47b8
unknown <walid.elkassem@isento.de>
2023-04-25 15:17:51 +0200

    update head component

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RosService } from '../shared/ros.service';
 import { MotorCurrentMessage } from '../shared/currentMessage';
 
@@ -7,39 +7,40 @@ import { MotorCurrentMessage } from '../shared/currentMessage';
   templateUrl: './head.component.html',
   styleUrls: ['./head.component.css']
 })
-export class HeadComponent {
+export class HeadComponent implements OnInit {
 
 
   constructor(private rosService: RosService){}
+
+  ngOnInit(): void {
+    this.rosService.currentReceiver$.subscribe(message => {
+      for (let i = 0; i < this.currentConsumptionOfMotors.length; i++) {
+        if (message['motor'] === this.currentConsumptionOfMotors[i]['motor']) {
+          console.log('current value' + message['currentValue']);
+          this.currentConsumptionOfMotors[i]['value'] = message['currentValue'];
+        }
+      }
+    })
+  }
   
-  tiltForwardMotor =  { name: "index_right_stretch", label: "Setting the position of the Motor" }
+  tiltForwardMotor =  { name: "tilt_forward_motor", label: "Tilt Forward" }
 
-  tiltSideWaysMotor =  { name: "index_right_stretch", label: "Setting the position of the Motor" }
+  tiltSideWaysMotor =  { name: "tilt_sideways_motor", label: "Tilt Sideways" }
 
-  turnTheHeadMotor =  { name: "index_right_stretch", label: "Setting the position of the Motor" }
+  turnHeadMotor =  { name: "turn_head_motor", label: "Head Rotation" }
 
   currentConsumptionOfMotors = [
     { motor: 'currentConsumption', value: 1230 },
     { motor: 'secondCurrentCnsumption', value: 60 }
   ]
 
-
-   sendMotorSettingToRos(){
-
-
-
+  sendDummyMessage() {
       for (let i = 0; i < this.currentConsumptionOfMotors.length; i++) {
         const message: MotorCurrentMessage = {
           motor: this.currentConsumptionOfMotors[i]['motor'],
           currentValue: Math.floor(Math.random() * 2000)
         }
-
         this.rosService.sendMessage(message);
       }
     }
-
-
-
-
-
 }
