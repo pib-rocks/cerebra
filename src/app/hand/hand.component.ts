9c1444d1f3ab4843422a76542a79788376a07711
unknown <walid.elkassem@isento.de>
2023-05-02 15:24:22 +0200

    code formatted

--- src/app/hand/hand.component.ts
+++ src/app/hand/hand.component.ts
@@ -1,31 +1,40 @@
-import { Component, Input, OnInit, QueryList, ViewChild, ViewChildren , isDevMode} from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
-import { SliderComponent } from '../slider/slider.component';
-import { RosService } from '../shared/ros.service';
-import { MotorCurrentMessage } from '../shared/currentMessage';
-import { Subject } from 'rxjs';
+import {
+  Component,
+  Input,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+  isDevMode,
+} from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { ActivatedRoute, Params } from "@angular/router";
+import { SliderComponent } from "../slider/slider.component";
+import { RosService } from "../shared/ros.service";
+import { MotorCurrentMessage } from "../shared/currentMessage";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-hand',
-  templateUrl: './hand.component.html',
-  styleUrls: ['./hand.component.css']
+  selector: "app-hand",
+  templateUrl: "./hand.component.html",
+  styleUrls: ["./hand.component.css"],
 })
 export class HandComponent implements OnInit {
   @ViewChildren(SliderComponent) childComponents!: QueryList<SliderComponent>;
 
   @Input() side = "left";
-  messageReceiver$: Subject<MotorCurrentMessage> = new Subject<MotorCurrentMessage>;
+  messageReceiver$: Subject<MotorCurrentMessage> =
+    new Subject<MotorCurrentMessage>();
 
-  constructor(private route: ActivatedRoute, private rosService: RosService) { }
+  constructor(private route: ActivatedRoute, private rosService: RosService) {}
 
   leftSwitchControl = new FormControl(false);
   rightSwitchControl = new FormControl(false);
 
   leftHand = [
     { motor: "index_left_stretch", label: "Open/Close all fingers" },
-    { motor: "thumb_left_opposition", label: "Thumb opposition" }
-  ]
+    { motor: "thumb_left_opposition", label: "Thumb opposition" },
+  ];
 
   leftFingers = [
     { motor: "thumb_left_stretch", label: "Thumb" },
@@ -33,13 +42,13 @@ export class HandComponent implements OnInit {
     { motor: "index_left_stretch", label: "Index finger" },
     { motor: "middle_left_stretch", label: "Middle finger" },
     { motor: "ring_left_stretch", label: "Ring finger" },
-    { motor: "pinky_left_stretch", label: "Pinky finger" }
-  ]
+    { motor: "pinky_left_stretch", label: "Pinky finger" },
+  ];
 
   rightHand = [
     { motor: "index_right_stretch", label: "Open/Close all fingers" },
-    { motor: "thumb_right_opposition", label: "Thumb opposition" }
-  ]
+    { motor: "thumb_right_opposition", label: "Thumb opposition" },
+  ];
 
   rightFingers = [
     { motor: "thumb_right_stretch", label: "Thumb" },
@@ -47,121 +56,135 @@ export class HandComponent implements OnInit {
     { motor: "index_right_stretch", label: "Index finger" },
     { motor: "middle_right_stretch", label: "Middle finger" },
     { motor: "ring_right_stretch", label: "Ring finger" },
-    { motor: "pinky_right_stretch", label: "Pinky finger" }
-  ]
+    { motor: "pinky_right_stretch", label: "Pinky finger" },
+  ];
 
   currentRight = [
-    { motor: 'pinky-right', value: 500 },
-    { motor: 'ring-right', value: 100 },
-    { motor: 'middle-right', value: 1000 },
-    { motor: 'index-right', value: 2000 },
-    { motor: 'thumb-right', value: 1500 }
-  ]
+    { motor: "pinky-right", value: 500 },
+    { motor: "ring-right", value: 100 },
+    { motor: "middle-right", value: 1000 },
+    { motor: "index-right", value: 2000 },
+    { motor: "thumb-right", value: 1500 },
+  ];
 
   currentLeft = [
-    { motor: 'pinky-left', value: 1600 },
-    { motor: 'ring-left', value: 1000 },
-    { motor: 'middle-left', value: 1700 },
-    { motor: 'index-left', value: 500 },
-    { motor: 'thumb-left', value: 300 }
-  ]
+    { motor: "pinky-left", value: 1600 },
+    { motor: "ring-left", value: 1000 },
+    { motor: "middle-left", value: 1700 },
+    { motor: "index-left", value: 500 },
+    { motor: "thumb-left", value: 300 },
+  ];
 
   ngOnInit(): void {
-    if(isDevMode()){
-      console.log('Development!');
+    if (isDevMode()) {
+      console.log("Development!");
     } else {
-      console.log('Production!');
+      console.log("Production!");
     }
     this.route.params.subscribe((params: Params) => {
-      this.side = params['side'];
+      this.side = params["side"];
     });
-    this.rosService.currentReceiver$.subscribe(message => {
+    this.rosService.currentReceiver$.subscribe((message) => {
       for (let i = 0; i < this.currentLeft.length; i++) {
-        if (message['motor'] === this.currentLeft[i]['motor']) {
-          console.log('current value' + message['currentValue']);
-          this.currentLeft[i]['value'] = message['currentValue'];
+        if (message["motor"] === this.currentLeft[i]["motor"]) {
+          console.log("current value" + message["currentValue"]);
+          this.currentLeft[i]["value"] = message["currentValue"];
         }
       }
       for (let i = 0; i < this.currentRight.length; i++) {
-        if (message['motor'] === this.currentRight[i]['motor']) {
-          console.log('current value' + message['currentValue']);
-          this.currentRight[i]['value'] = message['currentValue'];
+        if (message["motor"] === this.currentRight[i]["motor"]) {
+          console.log("current value" + message["currentValue"]);
+          this.currentRight[i]["value"] = message["currentValue"];
         }
       }
-    })
+    });
   }
 
   reset() {
-    this.childComponents.forEach(child => {
+    this.childComponents.forEach((child) => {
       if (child.sliderFormControl.value != 0) {
         child.sliderFormControl.setValue("0");
         child.sendMessage();
       }
-    })
+    });
   }
 
   sendDummyMessage() {
-    if (this.side === 'left') {
+    if (this.side === "left") {
       for (let i = 0; i < this.currentLeft.length; i++) {
         const message: MotorCurrentMessage = {
-          motor: this.currentLeft[i]['motor'],
-          currentValue: Math.floor(Math.random() * 2000)
-        }
+          motor: this.currentLeft[i]["motor"],
+          currentValue: Math.floor(Math.random() * 2000),
+        };
         this.rosService.sendMessage(message);
       }
     }
 
-    if (this.side === 'right') {
+    if (this.side === "right") {
       for (let i = 0; i < this.currentRight.length; i++) {
         const message: MotorCurrentMessage = {
-          motor: this.currentRight[i]['motor'],
-          currentValue: Math.floor(Math.random() * 2000)
-        }
+          motor: this.currentRight[i]["motor"],
+          currentValue: Math.floor(Math.random() * 2000),
+        };
         this.rosService.sendMessage(message);
       }
     }
   }
 
   switchView(side: string) {
-    const switchControl = side === 'left' ? this.leftSwitchControl : this.rightSwitchControl;
+    const switchControl =
+      side === "left" ? this.leftSwitchControl : this.rightSwitchControl;
     if (switchControl.value === true) {
-      const indexFinger = this.childComponents.filter(child => child.labelName === "Index finger")[0];
-      const thumbOppo = this.childComponents.filter(child => child.labelName === "Thumb opposition")[0];
-      console.log('switchView childComponents length')
+      const indexFinger = this.childComponents.filter(
+        (child) => child.labelName === "Index finger"
+      )[0];
+      const thumbOppo = this.childComponents.filter(
+        (child) => child.labelName === "Thumb opposition"
+      )[0];
+      console.log("switchView childComponents length");
       console.log(this.childComponents.length);
-      this.childComponents.forEach(child => {
+      this.childComponents.forEach((child) => {
         if (child.labelName != "Thumb opposition") {
           child.sliderFormControl.setValue(indexFinger.sliderFormControl.value);
           child.motorFormControl.setValue(indexFinger.motorFormControl.value);
-          child.velocityFormControl.setValue(indexFinger.velocityFormControl.value);
-          child.accelerationFormControl.setValue(indexFinger.accelerationFormControl.value);
-          child.decelerationFormControl.setValue(indexFinger.decelerationFormControl.value);
+          child.velocityFormControl.setValue(
+            indexFinger.velocityFormControl.value
+          );
+          child.accelerationFormControl.setValue(
+            indexFinger.accelerationFormControl.value
+          );
+          child.decelerationFormControl.setValue(
+            indexFinger.decelerationFormControl.value
+          );
           child.periodFormControl.setValue(indexFinger.periodFormControl.value);
           child.pulseMaxRange.setValue(indexFinger.pulseMaxRange.value);
           child.pulseMinRange.setValue(indexFinger.pulseMinRange.value);
           child.degreeMax.setValue(indexFinger.degreeMax.value);
           child.degreeMin.setValue(indexFinger.degreeMin.value);
         } else {
-          child.sliderFormControl.setValue(thumbOppo.sliderFormControl.value)
-          child.motorFormControl.setValue(thumbOppo.motorFormControl.value)
-          child.velocityFormControl.setValue(thumbOppo.velocityFormControl.value)
-          child.accelerationFormControl.setValue(thumbOppo.accelerationFormControl.value)
-          child.decelerationFormControl.setValue(thumbOppo.decelerationFormControl.value)
-          child.periodFormControl.setValue(thumbOppo.periodFormControl.value)
-          child.pulseMaxRange.setValue(thumbOppo.pulseMaxRange.value)
-          child.pulseMinRange.setValue(thumbOppo.pulseMinRange.value)
-          child.degreeMax.setValue(thumbOppo.degreeMax.value)
-          child.degreeMin.setValue(thumbOppo.degreeMin.value)
-
+          child.sliderFormControl.setValue(thumbOppo.sliderFormControl.value);
+          child.motorFormControl.setValue(thumbOppo.motorFormControl.value);
+          child.velocityFormControl.setValue(
+            thumbOppo.velocityFormControl.value
+          );
+          child.accelerationFormControl.setValue(
+            thumbOppo.accelerationFormControl.value
+          );
+          child.decelerationFormControl.setValue(
+            thumbOppo.decelerationFormControl.value
+          );
+          child.periodFormControl.setValue(thumbOppo.periodFormControl.value);
+          child.pulseMaxRange.setValue(thumbOppo.pulseMaxRange.value);
+          child.pulseMinRange.setValue(thumbOppo.pulseMinRange.value);
+          child.degreeMax.setValue(thumbOppo.degreeMax.value);
+          child.degreeMin.setValue(thumbOppo.degreeMin.value);
         }
         child.sendAllMessagesCombined();
       });
     } else {
-      this.childComponents.forEach(child => {
+      this.childComponents.forEach((child) => {
         child.sendAllMessagesCombined();
       });
     }
   }
-
-
 }
--- src/app/shared/ros.service.ts
+++ src/app/shared/ros.service.ts
@@ -1,33 +1,33 @@
-import { Injectable } from '@angular/core';
-import * as ROSLIB from 'roslib';
-import { BehaviorSubject, Subject } from 'rxjs';
-import { Message } from './message';
-import { Motor } from './motor';
-import { VoiceAssistant } from './voice-assistant';
-import { MotorCurrentMessage } from './currentMessage';
+import { Injectable } from "@angular/core";
+import * as ROSLIB from "roslib";
+import { BehaviorSubject, Subject } from "rxjs";
+import { Message } from "./message";
+import { Motor } from "./motor";
+import { VoiceAssistant } from "./voice-assistant";
+import { MotorCurrentMessage } from "./currentMessage";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class RosService {
   private isInitializedSubject = new BehaviorSubject<boolean>(false);
   isInitialized$ = this.isInitializedSubject.asObservable();
-  currentReceiver$: Subject<MotorCurrentMessage> = new Subject<MotorCurrentMessage>;
+  currentReceiver$: Subject<MotorCurrentMessage> =
+    new Subject<MotorCurrentMessage>();
   private ros!: ROSLIB.Ros;
   private messageTopic!: ROSLIB.Topic;
   private voiceAssistantTopic!: ROSLIB.Topic;
   private motorCurrentTopic!: ROSLIB.Topic;
-  private readonly topicName = '/motor_settings';
-  private readonly topicVoiceName = '/cerebra_voice_settings';
-  private readonly topicCurrentName = '/motor_status';
+  private readonly topicName = "/motor_settings";
+  private readonly topicVoiceName = "/cerebra_voice_settings";
+  private readonly topicCurrentName = "/motor_status";
 
   private motors: Motor[] = [];
 
   constructor() {
-   
     this.ros = this.setUpRos();
-    this.ros.on('connection', () => {
-      console.log('Connected to ROS');
+    this.ros.on("connection", () => {
+      console.log("Connected to ROS");
       this.isInitializedSubject.next(true);
       this.messageTopic = this.createMessageTopic();
       this.voiceAssistantTopic = this.createVoiceAssistantTopic();
@@ -35,23 +35,19 @@ export class RosService {
       this.subscribeTopic();
       this.subscribeCurrentTopic();
     });
-    this.ros.on('error', (error: string) => {
-      console.log('Error connecting to ROSBridge server:', error);
+    this.ros.on("error", (error: string) => {
+      console.log("Error connecting to ROSBridge server:", error);
     });
 
-    this.ros.on('close', () => {
-      console.log('Disconnected from ROSBridge server.');
+    this.ros.on("close", () => {
+      console.log("Disconnected from ROSBridge server.");
     });
   }
-
-
-
- 
 
   registerMotor(motorName: string, motorReceiver$: Subject<Message>) {
     let isRegistered = false;
     if (this.ros.isConnected) {
-      this.motors.forEach(m => {
+      this.motors.forEach((m) => {
         if (m.motor === motorName) {
           m.receiver$ = motorReceiver$;
           isRegistered = true;
@@ -61,45 +57,39 @@ export class RosService {
       if (!isRegistered) {
         const motor: Motor = {
           motor: motorName,
-          receiver$: motorReceiver$
-        }
+          receiver$: motorReceiver$,
+        };
         this.motors.push(motor);
       }
     }
   }
 
   sendMessage(msg: Message | VoiceAssistant | MotorCurrentMessage) {
-
     const json = JSON.parse(JSON.stringify(msg));
-    const parameters = Object.keys(json).map(key => ({ [key]: json[key] }));
-    const message = new ROSLIB.Message(
-      { data: JSON.stringify(parameters) }
-    );
-    if ('motor' in msg) {
-      if ('currentValue' in msg) {
-        
+    const parameters = Object.keys(json).map((key) => ({ [key]: json[key] }));
+    const message = new ROSLIB.Message({ data: JSON.stringify(parameters) });
+    if ("motor" in msg) {
+      if ("currentValue" in msg) {
         this.motorCurrentTopic?.publish(message);
-        console.log('Sent message ' + JSON.stringify(message));
+        console.log("Sent message " + JSON.stringify(message));
       } else {
         this.messageTopic?.publish(message);
-        console.log('Sent message ' + JSON.stringify(message));
+        console.log("Sent message " + JSON.stringify(message));
       }
     } else {
       this.voiceAssistantTopic.publish(message);
-      console.log('Sent message ' + JSON.stringify(message));
+      console.log("Sent message " + JSON.stringify(message));
     }
   }
 
   getReceiversByMotorName(motorName: string): Subject<Message>[] {
-    const foundMotors = this.motors.filter(m => m.motor === motorName);
-    return foundMotors.length > 0
-      ? foundMotors.map(m => m['receiver$'])
-      : [];
+    const foundMotors = this.motors.filter((m) => m.motor === motorName);
+    return foundMotors.length > 0 ? foundMotors.map((m) => m["receiver$"]) : [];
   }
 
   setUpRos() {
     return new ROSLIB.Ros({
-      url: 'ws://192.168.220.38:9090',
+      url: "ws://192.168.220.38:9090",
     });
   }
 
@@ -107,29 +97,39 @@ export class RosService {
     this.messageTopic.subscribe((message) => {
       const jsonStr = JSON.stringify(message);
       const json = JSON.parse(jsonStr);
-      const jsonArray = JSON.parse(json['data']);
+      const jsonArray = JSON.parse(json["data"]);
       const jsonObject = jsonArray.reduce((key: object, value: object) => {
         return { ...key, ...value };
       }, {});
-      console.log('Received message for ' + jsonObject['motor'] + ': ' + JSON.stringify(jsonObject));
-      const receivers$ = this.getReceiversByMotorName(jsonObject['motor']);
-      receivers$.forEach(r => {
+      console.log(
+        "Received message for " +
+          jsonObject["motor"] +
+          ": " +
+          JSON.stringify(jsonObject)
+      );
+      const receivers$ = this.getReceiversByMotorName(jsonObject["motor"]);
+      receivers$.forEach((r) => {
         r.next(jsonObject);
       });
-    })
+    });
   }
 
   subscribeCurrentTopic() {
     this.motorCurrentTopic.subscribe((message) => {
       const jsonStr = JSON.stringify(message);
       const json = JSON.parse(jsonStr);
-      const jsonArray = JSON.parse(json['data']);
+      const jsonArray = JSON.parse(json["data"]);
       const jsonObject = jsonArray.reduce((key: object, value: object) => {
         return { ...key, ...value };
       }, {});
-      console.log('Received message for ' + jsonObject['motor'] + ': ' + JSON.stringify(jsonObject));
+      console.log(
+        "Received message for " +
+          jsonObject["motor"] +
+          ": " +
+          JSON.stringify(jsonObject)
+      );
       this.currentReceiver$.next(jsonObject);
-    })
+    });
   }
 
   get Ros(): ROSLIB.Ros {
@@ -144,16 +144,15 @@ export class RosService {
     return new ROSLIB.Topic({
       ros: this.ros,
       name: this.topicName,
-      messageType: 'std_msgs/String'
+      messageType: "std_msgs/String",
     });
   }
-
 
   createVoiceAssistantTopic(): ROSLIB.Topic {
     return new ROSLIB.Topic({
       ros: this.ros,
       name: this.topicVoiceName,
-      messageType: 'std_msgs/String'
+      messageType: "std_msgs/String",
     });
   }
 
@@ -161,10 +160,7 @@ export class RosService {
     return new ROSLIB.Topic({
       ros: this.ros,
       name: this.topicCurrentName,
-      messageType: 'std_msgs/String'
+      messageType: "std_msgs/String",
     });
   }
-
 }
-
-
