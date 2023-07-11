3b3eba549568d54a7fc1f3a6e325d2265cdb4353
Christopher <christopher.fries@isento.de>
2023-07-11 16:24:18 +0200

    Added some spec tests for SliderComponent

--- src/app/slider/slider.component.spec.ts
+++ src/app/slider/slider.component.spec.ts
@@ -1,20 +1,26 @@
 import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { ReactiveFormsModule} from "@angular/forms";
 import { SliderComponent } from './slider.component';
+import { RosService } from "../shared/ros.service";
+import { Message } from "roslib";
 
 describe('SliderComponent', () => {
   let component: SliderComponent;
   let fixture: ComponentFixture<SliderComponent>;
+  let rosService: RosService;
+  let spySendMessage: jasmine.Spy<(msg: Message ) => void>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ SliderComponent ],
-      imports: [ ReactiveFormsModule ]
+      imports: [ ReactiveFormsModule ], 
+      providers: [RosService]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(SliderComponent);
     component = fixture.componentInstance;
+    // spySendMessage = spyOn(rosService, "sendSliderMessage");
     fixture.detectChanges();
   });
 
@@ -22,10 +28,36 @@ describe('SliderComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call sendMessage after init',fakeAsync(() => {
-    const spy = spyOn(component, 'sendMessage');
+  it("should change value after receiving a message", () => {
+    const slider = fixture.nativeElement.querySelector('input[type="range"]');
+    const jsonValue = String((component.minValue + component.maxValue)/2);
+    const json = {
+      motor: String(component.sliderName),
+      value: jsonValue,
+    };
+    component.messageReceiver$.next(json);
+    fixture.detectChanges();
+    expect(slider.value).toBe(jsonValue);
+  });
+
+  it("should call sendMessage after changing the slider", fakeAsync(() => {
+    const spyOnsendMessage = spyOn(component, "sendMessage");
+    const slider = fixture.nativeElement.querySelector('input[type="range"]');
+    slider.value = String((component.minValue + component.maxValue)/2);
+    slider.dispatchEvent(new Event('input'));
+    tick(1000);
+    expect(spyOnsendMessage).toHaveBeenCalled();
+  }));
+
+  it("should adjust the slider when using textinput on bubble", fakeAsync(() => {
     component.ngOnInit();
+    component.ngAfterViewInit();
+    const val = String((component.minValue + component.maxValue)/2);
+    component.bubbleFormControl.setValue(val);
+    component.bubbleInput = jasmine.createSpyObj('nativeElement', {}, {property : 'property'});
+    component.bubbleInput.nativeElement = jasmine.createSpyObj('nativeElement', { select : 'select', focus : 'focus'});
+    component.toggleInputVisible();
     tick(500);
-    expect(spy).toHaveBeenCalled();
+    expect(component.sliderFormControl.value).toBeLessThanOrEqual(component.bubbleFormControl.value);
   }));
 });
