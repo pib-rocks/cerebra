71f0b6c11f3130b0600b49b0bd76986670dd6938
Christopher <christopher.fries@isento.de>
2023-08-23 12:59:02 +0200

    Input Sanitizing and Spec testing Vert Slider

--- src/app/vertical-slider/vertical-slider.component.ts
+++ src/app/vertical-slider/vertical-slider.component.ts
@@ -41,15 +41,38 @@ export class VerticalSliderComponent implements OnInit, AfterViewInit {
     }
     ngAfterViewInit(): void {
         this.rangeFormControl.valueChanges.subscribe((value) => {
-            const slider: ElementRef["nativeElement"] =
-                this.slider?.nativeElement;
-            const sliderPercentage: number =
-                (this.rangeFormControl.value / this.maxValue) * 100;
-            slider.style.setProperty(
-                "--pos-relative",
-                sliderPercentage.toString() + "%",
-            );
+            if (this.sanitizeValue()) {
+                const slider: ElementRef["nativeElement"] =
+                    this.slider?.nativeElement;
+                const sliderPercentage: number =
+                    (this.rangeFormControl.value / this.maxValue) * 100;
+                slider.style.setProperty(
+                    "--pos-relative",
+                    sliderPercentage.toString() + "%",
+                );
+            }
         });
         this.rangeFormControl.setValue(this.rangeFormControl.value);
+    }
+
+    sanitizeValue() {
+        if (this.rangeFormControl.hasError("required")) {
+            this.rangeFormControl.setValue(0);
+        } else if (this.rangeFormControl.hasError("pattern")) {
+            this.rangeFormControl.setValue(0);
+        } else if (this.rangeFormControl.hasError("min")) {
+            this.rangeFormControl.setValue(this.minValue);
+        } else if (this.rangeFormControl.hasError("max")) {
+            this.rangeFormControl.setValue(this.maxValue);
+        } else if (this.rangeFormControl.hasError("steppingError")) {
+            let intFormControl = Math.floor(this.rangeFormControl.value * 1000);
+            const moduloValue = intFormControl % Math.floor(this.step * 1000);
+            intFormControl -= moduloValue;
+            intFormControl /= 1000;
+            this.rangeFormControl.setValue(intFormControl);
+        } else {
+            return true;
+        }
+        return false;
     }
 }
