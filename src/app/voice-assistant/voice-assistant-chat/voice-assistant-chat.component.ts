d5205fa29c6caf2e50ab4d22ba411e97b3d7864a
Christopher <christopher.fries@isento.de>
2023-11-10 12:23:19 +0100

    feat(PR-346): Implement mock chat, Match display of components

--- src/app/voice-assistant/voice-assistant-chat/voice-assistant-chat.component.ts
+++ src/app/voice-assistant/voice-assistant-chat/voice-assistant-chat.component.ts
@@ -1,60 +1,151 @@
-import {Component} from "@angular/core";
+import {Component, OnInit, TemplateRef, ViewChild} from "@angular/core";
+import {FormControl, Validators} from "@angular/forms";
+import {Router, ActivatedRoute} from "@angular/router";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {BehaviorSubject, Observable} from "rxjs";
+import {SidebarElement} from "src/app/shared/interfaces/sidebar-element.interface";
+import {VoiceAssistantService} from "src/app/shared/services/voice-assistant.service";
+import {MockElement} from "src/app/voice-assistant/voice-assistant-chat/mock-element.class";
 
 @Component({
     selector: "app-voice-assistant-chat",
     templateUrl: "./voice-assistant-chat.component.html",
     styleUrls: ["./voice-assistant-chat.component.css"],
 })
-export class VoiceAssistantChatComponent {
-    tabTitle = "CHAT";
-    exportButtonHovered = false;
-    activeIcon = "../../assets/voice-assistant-svgs/chat/chat_active.svg";
-    chatTopics = [
-        {id: "", name: "Short stories", selected: true, hovered: false},
-        {id: "", name: "Nürnberg", selected: false, hovered: false},
-        {
-            id: "",
-            name: "Definition of AI today",
-            selected: false,
-            hovered: false,
-        },
+export class VoiceAssistantChatComponent implements OnInit {
+    @ViewChild("modalContent") modalContent: TemplateRef<any> | undefined;
+    personalityIcon: string = "../../assets/voice-assistant-svgs/chat/chat.svg";
+
+    nameFormControl: FormControl = new FormControl("");
+    subject!: Observable<SidebarElement[]>;
+
+    constructor(
+        private modalService: NgbModal,
+        private voiceAssistantService: VoiceAssistantService,
+        private router: Router,
+        private route: ActivatedRoute,
+    ) {}
+
+    // FIXME: mockObjects for allowing basic functionality, delete after implementing chatService and Chat
+    mockElements: MockElement[] = [
+        new MockElement("Nürnberg", "12345678-9abc-def0-1234-56789abc"),
+        new MockElement(
+            "Humanoide Roboter",
+            "22345678-9abc-def0-1234-56789abc",
+        ),
+        new MockElement(
+            "Mockige Mockelemente",
+            "32345678-9abc-def0-1234-56789abc",
+        ),
     ];
-    chatIconPaths = {
-        icon: "../../assets/voice-assistant-svgs/chat/chat.svg",
+    mockSubject: BehaviorSubject<SidebarElement[]> = new BehaviorSubject<
+        SidebarElement[]
+    >(this.mockElements);
+    //
+
+    ngOnInit() {
+        //FIXME: Implement chatService
+        // this.subject = this.chatService.getSubject();
+        this.subject = this.mockSubject;
+        this.nameFormControl.setValidators([
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(255),
+        ]);
+    }
+
+    showModal = (uuid?: string) => {
+        return this.modalService
+            .open(this.modalContent, {
+                ariaLabelledBy: "modal-basic-title",
+                size: "sm",
+                windowClass: "myCustomModalClass",
+                backdropClass: "myCustomBackdropClass",
+            })
+            .result.then(
+                (result) => {
+                    console.log(`Closed with: ${result}`);
+                },
+                () => {
+                    if (uuid) {
+                        this.editChat(uuid);
+                    } else {
+                        this.addChat();
+                    }
+                },
+            );
     };
-    chatIcon: string = "../../assets/voice-assistant-svgs/chat/chat.svg";
+
+    openAddModal = () => {
+        this.nameFormControl.setValue("");
+        this.showModal();
+    };
+
+    openEditModal = () => {
+        const uuid: string = this.router.parseUrl(this.router.url).root
+            .children["primary"].segments[2].path;
+        if (uuid) {
+            // FIXME: implement chatService
+            // const updateChat =
+            //     this.chatService.getPersonality(uuid);
+            // this.nameFormControl.setValue(updateChat?.name ?? "");
+            this.showModal(uuid);
+        }
+    };
+
+    addChat() {
+        if (this.nameFormControl.valid) {
+            // FIXME: implement chatService, implement Chat
+            // this.chatService.createChat(
+            //     new Chat(
+            //         "",
+            //         this.nameFormControl.value,
+            //     ),
+            // );
+        }
+        throw Error("not implemented");
+    }
+
+    editChat = (uuid: string) => {
+        // FIXME: implement chatService, implement Chat
+        // const updatePersonality = this.chatService
+        //     .getChat(uuid)
+        //     ?.clone();
+        // if (updatePersonality) {
+        //     updatePersonality.name = this.nameFormControl.value;
+        //     this.chatService.updateChatById(updatePersonality);
+        // }
+        throw Error("not implemented");
+    };
+
+    deleteChat = () => {
+        // FIXME: implement chatService, implement Chat
+        // this.chatService.deleteChatById(
+        //     this.router.parseUrl(this.router.url).root.children["primary"]
+        //         .segments[2].path,
+        // );
+        // this.router.navigate(
+        //     [this.chatService.chats[0].personalityId],
+        //     {relativeTo: this.route},
+        // );
+        throw Error("not implemented");
+    };
 
     headerElements = [
         {
             icon: "../../assets/voice-assistant-svgs/chat/chat_add.svg",
-            active_icon:
-                "../../assets/voice-assistant-svgs/chat/chat_add_active.svg",
             label: "ADD",
-            hovered: false,
+            clickCallback: this.openAddModal,
         },
         {
             icon: "../../assets/voice-assistant-svgs/chat/chat_delete.svg",
-            active_icon:
-                "../../assets/voice-assistant-svgs/chat/chat_delete_active.svg",
             label: "DELETE",
-            hovered: false,
+            clickCallback: this.deleteChat,
         },
         {
             icon: "../../assets/voice-assistant-svgs/chat/chat_edit.svg",
-            active_icon:
-                "../../assets/voice-assistant-svgs/chat/chat_edit_active.svg",
             label: "EDIT",
-            hovered: false,
+            clickCallback: this.openEditModal,
         },
     ];
-    sendButton: string = "M120-160v-240l320-80-320-80v-240l760 320-760 320Z";
-    message: string = "";
-    messages: string[] = [];
-
-    sendMessage = () => {
-        if (this.message.trim() != "") {
-            this.messages.push(this.message);
-        }
-        this.message = "";
-    };
 }
