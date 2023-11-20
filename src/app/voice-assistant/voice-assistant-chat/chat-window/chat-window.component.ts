38568a00773c86c17310b32b92016cf19c4429e7
Christopher <christopher.fries@isento.de>
2023-11-20 15:10:47 +0100

    feat(PR-350): Implement chat-window functionality and chat-resolver

--- src/app/voice-assistant/voice-assistant-chat/chat-window/chat-window.component.ts
+++ src/app/voice-assistant/voice-assistant-chat/chat-window/chat-window.component.ts
@@ -1,10 +1,35 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormControl} from "@angular/forms";
+import {ActivatedRoute, Params} from "@angular/router";
+import {throwError} from "rxjs";
+import {ChatService} from "src/app/shared/services/chat.service";
+import {Chat} from "src/app/shared/types/chat.class";
 
 @Component({
     selector: "app-chat-window",
     templateUrl: "./chat-window.component.html",
     styleUrls: ["./chat-window.component.css"],
 })
-export class ChatWindowComponent {
+export class ChatWindowComponent implements OnInit {
     sendButton: string = "M120-160v-240l320-80-320-80v-240l760 320-760 320Z";
+    chat?: Chat;
+    promptFormControl: FormControl = new FormControl("");
+
+    constructor(
+        private chatService: ChatService,
+        private route: ActivatedRoute,
+    ) {}
+
+    ngOnInit(): void {
+        this.chat = this.route.snapshot.data["chat"];
+        localStorage.setItem("chat", this.chat?.chatId ?? "");
+        this.route.params.subscribe((params: Params) => {
+            this.chat = this.chatService.getChat(params["uuid"]);
+            localStorage.setItem("chat", this.chat?.chatId ?? "");
+        });
+    }
+
+    sendMessage() {
+        throw Error("not implemented");
+    }
 }
---
+++ src/app/voice-assistant/voice-assistant-resolver/chat.resolver.ts
@@ -0,0 +1,19 @@
+import {inject} from "@angular/core";
+import {ResolveFn} from "@angular/router";
+import {Observable} from "rxjs";
+import {ApiService} from "src/app/shared/services/api.service";
+import {ChatService} from "src/app/shared/services/chat.service";
+import {UrlConstants} from "src/app/shared/services/url.constants";
+import {Chat} from "src/app/shared/types/chat.class";
+
+export const chatResolver: ResolveFn<Observable<any> | void> = (
+    route,
+): Observable<Chat> | void => {
+    if (inject(ChatService).chats.length == 0) {
+        if (route.url.length > 0) {
+            return inject(ApiService).get(
+                UrlConstants.CHAT + `/${route.url.pop()!.toString()}`,
+            );
+        }
+    }
+};
