de34aa3202f5a690cd55cfc0f6927ca24f1832c0
Aditya Pai <aditya.pai@isento.de>
2023-12-07 16:15:21 +0100

    refactor: PR-445: Minor refactoring

--- src/app/shared/ros.service.ts
+++ src/app/shared/ros.service.ts
@@ -64,7 +64,7 @@ export class RosService {
             rosUrl = window.location.hostname;
         }
         return new ROSLIB.Ros({
-            url: `ws://${"127.0.0.1"}:9090`,
+            url: `ws://${rosUrl}:9090`,
         });
     }
 
