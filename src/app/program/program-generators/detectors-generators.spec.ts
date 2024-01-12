580f3d2dabec45e19452611204571b7308c05a3e
JaHe-isento <jan.hermann@isento.de>
2024-01-12 12:55:06 +0100

    test(PR-431): Add test for code generator

---
+++ src/app/program/program-generators/detectors-generators.spec.ts
@@ -0,0 +1,36 @@
+import {pythonGenerator} from "./detectors-generators";
+
+describe("pythonGenerator", () => {
+    it("should insert face detection code when the setting is 'start' and nothing when it's 'stop'", () => {
+        const block = jasmine.createSpyObj("block", ["getFieldValue"]);
+        const forBlock = jasmine.createSpyObj("forBlock", [
+            "face_detector_start_stop",
+        ]);
+        const generator = jasmine.createSpyObj(
+            "generator",
+            ["provideFunction_", "addReservedWords"],
+            ["forBlock"],
+        );
+        generator.definitions_ = {};
+        const className = "test-class-name";
+
+        block.getFieldValue.and.returnValue("START");
+        generator.provideFunction_.and.returnValue(className);
+        forBlock.face_detector_start_stop.and.returnValue(className);
+
+        const startCode = pythonGenerator.forBlock.face_detector_start_stop(
+            block,
+            generator,
+        );
+        expect(startCode).toContain(className);
+
+        block.getFieldValue.and.returnValue("STOP");
+        const stopCode = pythonGenerator.forBlock.face_detector_start_stop(
+            block,
+            generator,
+        );
+        expect(stopCode).not.toContain(className);
+
+        expect(block.getFieldValue).toHaveBeenCalledWith("SETTING");
+    });
+});
