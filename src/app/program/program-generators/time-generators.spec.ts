d411ee03961104380a46adccf1c99f65305e1716
JaHe-isento <jan.hermann@isento.de>
2024-01-24 11:41:15 +0100

    test(PR-501): Add test for time generators

---
+++ src/app/program/program-generators/time-generators.spec.ts
@@ -0,0 +1,29 @@
+import {pythonGenerator} from "blockly/python";
+
+describe("pythonGenerator", () => {
+    it("should build the time code string accordingly", () => {
+        const block = jasmine.createSpyObj("block", ["getFieldValue"]);
+
+        const generator = jasmine.createSpyObj("generator", ["forBlock"]);
+
+        generator.definitions_ = {};
+        block.getFieldValue.and.returnValue("time test value");
+
+        const code = pythonGenerator.forBlock.sleep_for_seconds(
+            block,
+            generator,
+        );
+        expect(code).toContain("time test value");
+
+        expect(block.getFieldValue).toHaveBeenCalledWith("SECONDS");
+    });
+
+    it("should generate code to get the system time", () => {
+        const generator = jasmine.createSpyObj("generator", ["forBlock"]);
+
+        generator.definitions_ = {};
+
+        const code = pythonGenerator.forBlock.get_system_time(generator);
+        expect(code).toContain("round(time.time() * 1000)");
+    });
+});
