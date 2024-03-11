ff328ec9c673e39c82398350b5bd6be4e011d25c
Nico Hertel <nico.hertel@isento.de>
2024-03-11 10:48:58 +0100

    fix(PR-609): do not display output of running program if switched to other program

--- src/app/program/program-splitscreen/console/console.component.ts
+++ src/app/program/program-splitscreen/console/console.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, SimpleChanges} from "@angular/core";
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 import {ProgramOutputLine} from "src/app/shared/ros-types/msg/program-output-line";
 import {ExecutionState, ProgramState} from "src/app/shared/types/program-state";
 
@@ -14,21 +14,29 @@ export class ConsoleComponent {
 
     ExecutionState = ExecutionState;
 
-    ngOnChanges(changes: SimpleChanges): void {
-        if ("output$" in changes) {
-            (
-                changes["output$"].currentValue as Observable<
-                    ProgramOutputLine[]
-                >
-            ).subscribe((output) => (this.output = output));
-        }
-        if ("state$" in changes) {
-            (
-                changes["state$"].currentValue as Observable<ProgramState>
-            ).subscribe((state) => (this.state = state));
-        }
-    }
-
     output: ProgramOutputLine[] = [];
     state: ProgramState = {executionState: ExecutionState.NOT_STARTED};
+
+    outputSubscription?: Subscription;
+    stateSubscription?: Subscription;
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if ("output$" in changes) {
+            this.outputSubscription?.unsubscribe();
+            const outputNext = changes["output$"].currentValue as Observable<
+                ProgramOutputLine[]
+            >;
+            this.outputSubscription = outputNext.subscribe(
+                (output) => (this.output = output),
+            );
+        }
+        if ("state$" in changes) {
+            this.stateSubscription?.unsubscribe();
+            const stateNext = changes["state$"]
+                .currentValue as Observable<ProgramState>;
+            this.stateSubscription = stateNext.subscribe(
+                (state) => (this.state = state),
+            );
+        }
+    }
 }
