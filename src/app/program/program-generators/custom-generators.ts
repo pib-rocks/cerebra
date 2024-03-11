6f5d2af49ed2a85de43ebeab7f6d40b62618a130
Alexander Meyer <alexander.meyer@isento.de>
2024-03-11 10:22:00 +0100

    feat: Create Blockly Blocks and Generators for motors

--- src/app/program/program-generators/custom-generators.ts
+++ src/app/program/program-generators/custom-generators.ts
@@ -2,12 +2,14 @@ import {pythonGenerator} from "blockly/python";
 
 import * as face_detector_blocks from "./detectors-generators";
 import * as time_blocks from "./time-generators";
+import * as motor_blocks from "./motor-generators";
 
 export * from "blockly/python";
 
 const generators: typeof pythonGenerator.forBlock = {
     ...face_detector_blocks,
     ...time_blocks,
+    ...motor_blocks,
 };
 
 for (const name in generators) {
