03ad70c749699a7f198856928f844bf6976a9f8d
Nico Hertel <nico.hertel@isento.de>
2024-03-13 14:29:58 +0100

    fix(PR-609): change ips back to localhost

--- src/app/shared/services/ros-service/ros.service.ts
+++ src/app/shared/services/ros-service/ros.service.ts
@@ -127,7 +127,7 @@ export class RosService implements IRosService {
     private setUpRos() {
         let rosUrl: string;
         if (isDevMode()) {
-            rosUrl = "192.168.220.148";
+            rosUrl = "127.0.0.1";
         } else {
             rosUrl = window.location.hostname;
         }
