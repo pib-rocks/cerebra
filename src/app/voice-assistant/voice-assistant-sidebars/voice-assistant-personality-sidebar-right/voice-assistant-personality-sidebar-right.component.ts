394bc09e2a4cad329637ec73531264a54c5d5757
Niklas Rothberger <niklas.rothberger@isento.de>
2024-03-14 14:11:47 +0100

    feat(PR-611): implement personality right sidebar logic

--- src/app/voice-assistant/voice-assistant-sidebars/voice-assistant-personality-sidebar-right/voice-assistant-personality-sidebar-right.component.ts
+++ src/app/voice-assistant/voice-assistant-sidebars/voice-assistant-personality-sidebar-right/voice-assistant-personality-sidebar-right.component.ts
@@ -1,29 +1,70 @@
-import {Component, Input} from "@angular/core";
-import {FormGroup} from "@angular/forms";
+import {Component, Input, OnInit} from "@angular/core";
 import {VoiceAssistant} from "src/app/shared/types/voice-assistant";
+import {VoiceAssistantService} from "src/app/shared/services/voice-assistant.service";
+import {ActivatedRoute, Params} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 @Component({
     selector: "app-va-personality-sidebar-right",
     templateUrl: "./voice-assistant-personality-sidebar-right.component.html",
     styleUrls: ["./voice-assistant-personality-sidebar-right.component.css"],
 })
-export class VoiceAssistantPersonalitySidebarRightComponent {
+export class VoiceAssistantPersonalitySidebarRightComponent implements OnInit {
+    pauseThresholdMin = 0.1;
+    pauseThresholdMax = 3.0;
+    nameMinLength = 2;
+    nameMaxLength = 255;
+    personalityClone!: VoiceAssistant;
+    thresholdString: string = "";
     personalityForm!: FormGroup;
-    thresholdString: string | undefined;
-    personalityName: string | undefined;
-    gender: string | undefined;
-    pauseThreshold: number | undefined;
-    @Input() personality?: VoiceAssistant;
+    @Input() personalityUUID: String | undefined;
+
+    constructor(
+        private voiceAssistantService: VoiceAssistantService,
+        private route: ActivatedRoute,
+    ) {}
 
     ngOnInit() {
-        this.personalityName = this.personality!.name;
-        this.gender = this.personality!.gender;
-        this.pauseThreshold = this.personality!.pauseThreshold;
+        this.personalityClone = this.route.snapshot.data["personality"];
+        this.route.params.subscribe((params: Params) => {
+            let temp = this.voiceAssistantService.getPersonality(
+                params["personalityUuid"],
+            );
+            if (temp !== undefined) {
+                this.personalityClone = temp;
+            }
+        });
+        this.thresholdString = this.personalityClone.pauseThreshold + "s";
+
+        this.personalityForm = new FormGroup({
+            "persona-name": new FormControl(this.personalityClone.name, {
+                nonNullable: true,
+                validators: [
+                    Validators.required,
+                    Validators.minLength(this.nameMinLength),
+                    Validators.maxLength(this.nameMaxLength),
+                ],
+            }),
+            gender: new FormControl(this.personalityClone.gender, {
+                nonNullable: true,
+                validators: [Validators.required],
+            }),
+            pausethreshold: new FormControl(
+                this.personalityClone.pauseThreshold,
+                {
+                    nonNullable: true,
+                    validators: [
+                        Validators.required,
+                        Validators.min(this.pauseThresholdMin),
+                        Validators.max(this.pauseThresholdMax),
+                    ],
+                },
+            ),
+        });
     }
 
     adjustThreshold(step: string) {
         const newValue =
-            (Number(this.personalityForm.controls["pausethreshold"].value) *
-                10 +
+            (Number(this.personalityClone.pauseThreshold) * 10 +
                 Number(step) * 10) /
             10;
         this.personalityForm.patchValue({
@@ -31,15 +72,43 @@ export class VoiceAssistantPersonalitySidebarRightComponent {
         });
         if (this.personalityForm.controls["pausethreshold"].hasError("min")) {
             this.personalityForm.patchValue({
-                pausethreshold: 0.1,
+                pausethreshold: this.pauseThresholdMin,
             });
         }
         if (this.personalityForm.controls["pausethreshold"].hasError("max")) {
             this.personalityForm.patchValue({
-                pausethreshold: 3,
+                pausethreshold: this.pauseThresholdMax,
             });
         }
         this.thresholdString =
             this.personalityForm.controls["pausethreshold"].value + "s";
+        this.personalityClone.pauseThreshold =
+            this.personalityForm.controls["pausethreshold"].value;
+    }
+
+    deletePersonality = () => {
+        if (
+            this.personalityClone!.getUUID() &&
+            this.voiceAssistantService.personalities.length > 0
+        ) {
+            this.voiceAssistantService.deletePersonalityById(
+                this.personalityClone!.personalityId,
+            );
+        }
+    };
+
+    updatePersonality() {
+        if (this.personalityForm.valid) {
+            this.personalityClone.name =
+                this.personalityForm.controls["persona-name"].value;
+            this.personalityClone.gender =
+                this.personalityForm.controls["gender"].value;
+            this.voiceAssistantService.updatePersonalityById(
+                this.personalityClone!,
+            );
+        } else {
+            // @TODO Throw error
+            console.log("Persona could not be saved, invalid input");
+        }
     }
 }
