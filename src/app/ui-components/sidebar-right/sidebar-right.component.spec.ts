8cdf3bbf6e6ed397682c03ab34911575cfe6fea9
Nino Meisinger <nino.meisinger@isento.de>
2024-03-25 16:35:46 +0100

    fix(PR-611): fix sidebar-right unit tests

--- src/app/ui-components/sidebar-right/sidebar-right.component.spec.ts
+++ src/app/ui-components/sidebar-right/sidebar-right.component.spec.ts
@@ -1,54 +1,76 @@
-// import {
-//     ComponentFixture,
-//     TestBed,
-//     fakeAsync,
-//     tick,
-// } from "@angular/core/testing";
+import {
+    ComponentFixture,
+    TestBed,
+    fakeAsync,
+    tick,
+} from "@angular/core/testing";
 
-// import {SideBarRightComponent} from "./sidebar-right.component";
-// import {RouterTestingModule} from "@angular/router/testing";
-// import {BehaviorSubject} from "rxjs";
-// import {SidebarElement} from "src/app/shared/interfaces/sidebar-element.interface";
-// import {VoiceAssistant} from "src/app/shared/types/voice-assistant";
-// import {ActivatedRoute} from "@angular/router";
+import {SideBarRightComponent} from "./sidebar-right.component";
+import {RouterTestingModule} from "@angular/router/testing";
+import {BehaviorSubject} from "rxjs";
+import {SidebarElement} from "src/app/shared/interfaces/sidebar-element.interface";
+import {VoiceAssistant} from "src/app/shared/types/voice-assistant";
+import {ActivatedRoute} from "@angular/router";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
-// describe("SideBarRightComponent", () => {
-//     let component: SideBarRightComponent;
-//     let fixture: ComponentFixture<SideBarRightComponent>;
-//     const testSubject = new BehaviorSubject<SidebarElement[]>([
-//         new VoiceAssistant("123", "Test", "Male", 0.8, ""),
-//     ]);
-//     beforeEach(async () => {
-//         await TestBed.configureTestingModule({
-//             declarations: [SideBarRightComponent],
-//             providers: [
-//                 {
-//                     provide: ActivatedRoute,
-//                     useValue: {},
-//                 },
-//             ],
-//             imports: [RouterTestingModule],
-//         }).compileComponents();
+describe("SideBarRightComponent", () => {
+    let component: SideBarRightComponent;
+    let fixture: ComponentFixture<SideBarRightComponent>;
+    const testSubject = new BehaviorSubject<SidebarElement[]>([
+        new VoiceAssistant("123", "Test", "Male", 0.8, ""),
+    ]);
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SideBarRightComponent],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {},
+                },
+            ],
+            imports: [RouterTestingModule, HttpClientTestingModule],
+        }).compileComponents();
 
-//         fixture = TestBed.createComponent(SideBarRightComponent);
-//         component = fixture.componentInstance;
-//         component.subject = testSubject;
-//         component.lStorage = "temp";
-//         fixture.detectChanges();
-//     });
+        fixture = TestBed.createComponent(SideBarRightComponent);
+        component = fixture.componentInstance;
+        component.subject = testSubject;
+        component.lStorage = "temp";
 
-//     it("should create", () => {
-//         expect(component).toBeTruthy();
-//     });
+        component.callbackMethods = [
+            {
+                icon: "",
+                label: "New program",
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                clickCallback: () => {},
+            },
+            {
+                icon: "",
+                label: "Rename",
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                clickCallback: () => {},
+            },
+            {
+                icon: "",
+                label: "Delete",
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                clickCallback: () => {},
+            },
+        ];
+        fixture.detectChanges();
+    });
 
-//     it("should assume new values when subscribed Observable emits new value", fakeAsync(() => {
-//         component.ngOnInit();
-//         tick(200);
-//         expect(component.sidebarElements.length).toBe(1);
-//         testSubject.next([
-//             new VoiceAssistant("321", "ABC", "Female", 0.3),
-//             new VoiceAssistant("322", "ADC", "Female", 0.4),
-//         ]);
-//         expect(component.sidebarElements.length).toBe(2);
-//     }));
-// });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it("should assume new values when subscribed Observable emits new value", fakeAsync(() => {
+        component.ngOnInit();
+        tick(200);
+        expect(component.sidebarElements.length).toBe(1);
+        testSubject.next([
+            new VoiceAssistant("321", "ABC", "Female", 0.3),
+            new VoiceAssistant("322", "ADC", "Female", 0.4),
+        ]);
+        expect(component.sidebarElements.length).toBe(2);
+    }));
+});
