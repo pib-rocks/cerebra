ca3ccc05298477c412d9ffdfebbffab61849c5da
Nico Hertel <nico.hertel@isento.de>
2024-04-19 16:23:27 +0200

    fix(PR-620): replace python-logging with ros-logging

--- src/app/program/program-generators/play-audio-from-speech-generator.ts
+++ src/app/program/program-generators/play-audio-from-speech-generator.ts
@@ -28,13 +28,13 @@ export function playAudioFromSpeechGenerator(
             super().__init__('play_audio_from_speech_client')
     
             self.client = self.create_client(PlayAudioFromSpeech, 'play_audio_from_speech')
-            logging.info(f"waiting for 'play_audio_from_speech' service to become available...")
+            self.get_logger().info(f"waiting for 'play_audio_from_speech' service to become available...")
             self.client.wait_for_service()
-            logging.info(f"service now available")
+            self.get_logger().info(f"service now available")
     
         def play_audio_from_speech(self, speech: str, voice: str) -> None:
     
-            logging.info(f"received request to say '{speech}' as '{voice}'.")
+            self.get_logger().info(f"received request to say '{speech}' as '{voice}'.")
     
             request = PlayAudioFromSpeech.Request()
             request.speech = speech
@@ -53,23 +53,17 @@ export function playAudioFromSpeechGenerator(
                 request.gender = "Male"
                 request.language = "English"
             else:
-                logging.error(f"unrecognized voice: '{voice}', aborting...")
+                self.get_logger().error(f"unrecognized voice: '{voice}', aborting...")
                 return
 
             future = self.client.call_async(request)
 
-            logging.info(f"now speaking...")
+            self.get_logger().info(f"now speaking...")
             rclpy.spin_until_future_complete(self, future)
-            logging.info("finished speaking.")
+            self.get_logger().info("finished speaking.")
 `,
     );
 
-    (generator as any).definitions_["import_logging"] = "import logging";
-    (generator as any).definitions_[
-        "logging_basicConfig"
-    ] = `logging.basicConfig(
-            level=logging.INFO, 
-            format="[%(levelname)s] [%(asctime)s]: %(message)s")`;
     (generator as any).definitions_["import_rclpy"] = "import rclpy";
     (generator as any).definitions_["from_rclpy_node_import_Node"] =
         "from rclpy.node import Node";
