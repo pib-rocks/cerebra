e3c96cbaa9bb68a1211072c7f2cae47de85c230a
Jonas Poller <jonas.poller@isento.de>
2024-04-30 13:14:13 +0200

    feat(AdjustMinorDesignIssues): Adjusted variable names of motorsettings vertical slider inputs to lowercase

--- src/app/sliders/vertical-slider/vertical-slider.component.ts
+++ src/app/sliders/vertical-slider/vertical-slider.component.ts
@@ -38,6 +38,7 @@ export class VerticalSliderComponent implements OnInit, AfterViewInit {
     valueSanitized: boolean = false;
 
     ngOnInit(): void {
+        this.name = this.name?.toLowerCase();
         this.rangeFormControl.setValue(this.defaultValue);
         this.messageReceiver$.subscribe((value: number) => {
             this.rangeFormControl.setValue(value);
