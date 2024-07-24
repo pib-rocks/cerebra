2b3efac1dcab289c11ae5f8d1f39cbaed9468607
nmsger-isento <159000582+nmsger-isento@users.noreply.github.com>
2024-07-24 14:28:15 +0200

    Revert "PR-730 => develop"

--- src/app/voice-assistant/voice-assistant-chat/chat-window/chat-window.component.spec.ts
+++ src/app/voice-assistant/voice-assistant-chat/chat-window/chat-window.component.spec.ts
@@ -30,7 +30,6 @@ describe("ChatWindowComponent", () => {
                 "sendChatMessage",
                 "getChat",
                 "getIsListeningObservable",
-                "filterMessageUpdates",
             ]);
         await TestBed.configureTestingModule({
             declarations: [ChatWindowComponent],
@@ -69,20 +68,6 @@ describe("ChatWindowComponent", () => {
         chatService.getIsListeningObservable.and.returnValue(
             isListeningSubject,
         );
-        chatService.filterMessageUpdates.and.returnValue([
-            {
-                messageId: "message-id-1",
-                timestamp: "yesterday",
-                isUser: false,
-                content: "hello world",
-            },
-            {
-                messageId: "message-id-2",
-                timestamp: "today",
-                isUser: false,
-                content: "hello world",
-            },
-        ]);
 
         fixture = TestBed.createComponent(ChatWindowComponent);
         component = fixture.componentInstance;
