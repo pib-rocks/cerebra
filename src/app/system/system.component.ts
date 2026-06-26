b768a4f6df41146ca69ffa7349d60f52c669deec
jueba <juerphen@gmail.com>
2026-06-26 19:13:55 +0200

    add System-item to main menu with setting Hardware-IDs as first functionality

---
+++ src/app/system/system.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "app-system",
+    templateUrl: "./system.component.html",
+    styleUrls: ["./system.component.scss"],
+})
+export class SystemComponent {}
